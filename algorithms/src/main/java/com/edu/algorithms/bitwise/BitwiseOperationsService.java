42d164a692474f4bd58612492ff91f542d4966a2
aramis <artesumen@gmail.com>
2022-10-25 17:20:36 +0300

    Intial Commit

---
+++ algorithms/src/main/java/com/edu/algorithms/bitwise/BitwiseOperationsService.java
@@ -0,0 +1,45 @@
+package com.edu.algorithms.bitwise;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BitwiseOperationsService {
+
+    /**
+     * Counts the number of bits in integer
+     * Time O(n); n - number of bits in input
+     *
+     *  @param input integer arg
+     * @return number of bits
+     */
+    public short suboptimalCountBits(int input) {
+        short numOfBits = 0;
+        while (input != 0) {
+            numOfBits += input & 1; // Логическое "И" даст 1, если крайний правый бит = 1, иначе даст 0
+            input >>>= 1; // Сдвиг всех битов на одну позицию вправо. Свободные позиции слева заменяем нулями
+        }
+        return numOfBits;
+    }
+
+    /**
+     * Finds the lowermost bit (the rightest '1') in input. Sets to '0'
+     *
+     * @param input integer arg
+     * @return Value with updated value of the lowermost bit
+     */
+    public int clearLowermostBit(int input) {
+        return input & (input - 1);
+    }
+
+    /**
+     * Builds a mask and gets a bit out of the input
+     *
+     * @param input integer arg
+     * @param position position to test
+     * @return true if the bit was present on a position, false if not
+     */
+    public boolean showBitOnThePosition(int input, int position){
+        int mask = 1 << (position - 1); // Маска будет иметь единственный бит на указанной позиции
+        return (input & mask) != 0;
+    }
+}
---
+++ algorithms/src/test/java/com/edu/algorithms/bitwise/BitwiseOperationsServiceTest.java
@@ -0,0 +1,32 @@
+package com.edu.algorithms.bitwise;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitwiseOperationsServiceTest {
+
+    @Autowired
+    private BitwiseOperationsService bitwiseOperationsService;
+    private static final int BINARY_NUMBER_0 = 0b0001_0011_0010_1110;
+    private static final int BINARY_NUMBER_1 = 0b0001_0011_0010_1111;
+
+    @Test
+    void suboptimalCountBits() {
+        Assertions.assertEquals(8, bitwiseOperationsService.suboptimalCountBits(BINARY_NUMBER_1));
+    }
+
+    @Test
+    void clearLowermostBit() {
+        Assertions.assertEquals(BINARY_NUMBER_0,bitwiseOperationsService.clearLowermostBit(BINARY_NUMBER_1));
+    }
+
+    @Test
+    void showBitOnThePosition() {
+        System.out.println(1<<(5-1)); // 10000(2), 16(10), 5я позиция
+        Assertions.assertTrue(bitwiseOperationsService.showBitOnThePosition(BINARY_NUMBER_0, 2));
+        Assertions.assertFalse(bitwiseOperationsService.showBitOnThePosition(BINARY_NUMBER_0, 1));
+    }
+}
